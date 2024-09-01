79af07ae742acb5ee6284e063d9e56613206be7f
Amarasena B.H.L.M. <lahimalshi@gmail.com>
2024-09-01 23:17:47 +0530

    feat:update features

--- image-search-engine/src/components/Image.js
+++ image-search-engine/src/components/Image.js
@@ -1,7 +1,8 @@
 const Image = ({ data }) => {
     return (
-      <a href={data.urls.regular} target="_blank" rel="noreferrer">
+      <a href={data.urls.regular} target="_blank" rel="noreferrer" download={Image}>
         <img className="h-72 w-full object-cover rounded-lg shadow-md" src={data.urls.small} alt={data.alt_description} />
+        download
       </a>
     )
   }
